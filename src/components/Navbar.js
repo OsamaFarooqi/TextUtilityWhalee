d3d7c9f662218f04447d2cecfc0d1597752b6654
OsamaFarooqi <40718347+OsamaFarooqi@users.noreply.github.com>
2025-09-28 14:21:37 +0500

    Deployed on GitHub pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar({
       className={`navbar navbar-expand-lg bg-body-primary navbar-${mode} bg-${mode}`}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="#">
           {title}
         </a>
         <button
@@ -31,16 +31,16 @@ export default function Navbar({
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+            {/* <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a className="nav-link" href="#">
                 {aboutText}
               </a>
-            </li>
+            </li> */}
           </ul>
           {/* <form className="d-flex" role="search">
             <input
